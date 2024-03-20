a743b062d14160a7ae881e5ad8c0ecedd902fdc1
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2024-03-20 00:49:56 +0000

    This is joyful

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a')-1, -1):
+    if i % 2 == 0:
         print(f"{chr(i)}", end="")
-        print(f"{chr(ord(chr(i+1-32)))}", end="")
-
+    else:
+        print(f"{chr(ord(chr(i))-32)}", end="")
