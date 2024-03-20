84a563b6ca3f96a2915a8a6b4188d7b88e6d548f
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2024-03-20 22:57:22 +0000

    Contacting the fucking OS

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argv = sys.argv
-    length = len(argv) - 1
+
+    length = len(sys.argv) - 1
     if length == 1:
-        print("{} argument.".format(length))
+        print("1 argument.")
     elif length == 0:
-        print("{} arguments.".format(length))
+        print("0 arguments.")
     else:
         print("{} arguments:".format(length))
-    for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
+    for i in range(length):
+        print("{}: {}".format(i+1, sys.argv[i+1]))
