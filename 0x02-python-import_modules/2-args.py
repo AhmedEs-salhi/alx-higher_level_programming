162cf46211d181e446af3587429c30d0e8bd0201
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2024-03-20 22:27:21 +0000

    Contacting with the OS

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+length = len(sys.argv) - 1
+if length == 1:
+    print("{} argument".format(length))
+elif length == 0:
+    print("{} arguments.".format(length))
+else:
+    print("{} arguments:".format(length))
+for i in range(1, len(sys.argv)):
+    print("{} : {}".format(i, sys.argv[i]))
