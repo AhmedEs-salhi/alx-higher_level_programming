4d851d37d3a070815a37582447d1a941f2fe4cea
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2024-03-20 23:01:52 +0000

    Contacting the OS

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     length = len(sys.argv) - 1
     if length == 1:
-        print("1 argument.")
+        print("1 argument:")
     elif length == 0:
         print("0 arguments.")
     else:
