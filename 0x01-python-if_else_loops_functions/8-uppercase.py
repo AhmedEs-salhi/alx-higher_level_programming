d02095b841da600379a435f8097e62cacb3411a8
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2024-03-20 00:06:51 +0000

    i am exausted

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(string):
-    for i in string:
+    for i in string :
         if ord(i) >= 97 and ord(i) <= 122:
             print("{}".format(chr(ord(i)-32)), end="")
         else:
             print("{}".format(i), end="")
-    print("\n")
