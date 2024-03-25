847f41abccd86b92bcd9adfa58c0b4a7acf27441
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2024-03-25 00:26:50 +0000

    my commit

--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python3
-t = (1, 2)
-u = (3, 4)
+def ret(sent):
+    return t[0], t[1]
 
-if len(t) > 0:
-    a = t[0]
-else:
-    a = 0
-if len(t) > 1:
-    b = t[1]
-else:
-    b = 0
-if len(u) > 0:
-    c = u[0]
-else:
-    c = 0
-if len(u) > 1:
-    d = u[1]
-else:
-    d = 0
 
-print(a + c, b + d)
+a, b = ret((1, 2))
+print(a, b)
