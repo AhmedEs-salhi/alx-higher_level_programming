47cd3cbefb94b902e48bb0b75f1e4de09ad88dbd
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-07-08 20:10:40 +0100

    It print with format now!

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ counter = 0
 for i in range(0, 10):
     for j in range(i, 10):
         if i != j:
-            print(f"{i}{j}", end="")
+            print("{}{}".format(i, j), end="")
             if counter < 44:
                 print(", ", end="")
                 counter += 1
