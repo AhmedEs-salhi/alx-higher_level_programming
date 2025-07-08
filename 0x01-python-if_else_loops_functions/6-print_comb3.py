bbb7dde2a4ed07454e67c84523ea6d15691d0009
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-07-08 20:06:38 +0100

    Here is the combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+counter = 0
 for i in range(0, 10):
-    for j in range(i+1, 10):
-        if i == 8 and j == 9:
-            print("{}{}".format(i, j), end="\n")
-        else:
-            print("{}{}, ".format(i, j), end="")
+    for j in range(i, 10):
+        if i != j:
+            print(f"{i}{j}", end="")
+            if counter < 44:
+                print(", ", end="")
+                counter += 1
+print("\n", end="")
