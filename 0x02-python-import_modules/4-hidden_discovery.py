dde973a69eec8c34ed3d2b94d92151015332fbaa
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-07-10 21:38:56 +0100

    Finding the tricky part which was supposed to be hidden :)

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    __import__("hidden_4.pyc")
-    module_names = dir(hidden_4)
-    for name in sorted(module_names):
-        if not name.starteswith('__'):
+    import hidden_4 as hidden
+    import re
+    for name in dir(hidden):
+        if not re.findall("__", name):
             print(name)
