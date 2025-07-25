c4ca8333f846c227db97c030a8c0259c3cf28d15
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-07-25 21:24:15 +0100

    Start enhancing my code

--- 0x04-python-more_data_structures/script.py
+++ 0x04-python-more_data_structures/script.py
@@ -2,3 +2,9 @@
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     return list(map(lambda x: replace if x == search else x, my_list))
+
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+new_list = search_replace(my_list, 2, 89)
+
+print(new_list)
+print(my_list)
