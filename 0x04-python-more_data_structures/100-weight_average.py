56c70a66ad7904c2b234c431f54387b433db0189
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-07-29 00:59:53 +0100

    Just Perfection

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,7 @@
 def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
-    up= 0
+    up = 0
     down = 0
     for tuple_element in my_list:
         tuple_list = list(tuple_element)
