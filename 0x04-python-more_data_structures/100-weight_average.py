1c688da1869e16a5b6134fa10c63dc8422d2f2db
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-07-29 00:56:23 +0100

    This is my commit

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-def weight_averge(my_list=[]):
+def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
-
+    up= 0
+    down = 0
+    for tuple_element in my_list:
+        tuple_list = list(tuple_element)
+        up += tuple_list[0] * tuple_list[1]
+        down += tuple_list[1]
+    return float(up / down)
--- 0x04-python-more_data_structures/script.py
+++ 0x04-python-more_data_structures/script.py
@@ -1,33 +1,11 @@
 #!/usr/bin/python3
-""" Test file
-"""
-roman_to_int = __import__('12-roman_to_int').roman_to_int
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
+temp_list = []
+addition = 0
+mult = 0
+for i in my_list:
+    temp_list = list(i)
+    mult += temp_list[0] * temp_list[1]
+    addition += temp_list[1]
 
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+print()
