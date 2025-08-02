bae85fa76f83fda5a7744ad08cc2147a50bd9fea
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-08-02 20:39:25 +0100

    Ignore what you don't have to pay attention at

--- 0x03-python-data_structures/script.py
+++ 0x03-python-data_structures/script.py
@@ -1,7 +1,14 @@
+#!/usr/bin/python3
 import sys
 
 def compare_strings(string_1, string_2):
+    """ Compare Two strings
+        Return True if both strings are identical
+        False otherwise.
+    """
     if string_1 == string_2:
         return True
     return False
-compare_strings(sys.argv[2], sys.argv[3])
+
+print(compare_strings.__doc__)
+compare_strings(sys.argv[1], sys.argv[2])
--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,6 +3,14 @@ import sys
 
 
 def safe_print_integer_err(value):
+    """_summary_
+
+    Args:
+        value (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     result = True
     try:
         print("{:d}".format(value))
