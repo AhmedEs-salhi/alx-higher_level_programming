765aa4a749e2ce44f91331f8820ac15358306091
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-08-02 01:15:06 +0100

    Learn C, you'll understand Python as a gift

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    result = True
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        result = False
+    finally:
+        return result
