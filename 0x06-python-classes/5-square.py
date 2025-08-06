fc72fb410755890dfa47956110e40346e1c4700c
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-08-06 16:46:51 +0100

    Now, we're start writing down our progress

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+""" This is the place where modules should be documented
+"""
+
+
+class Square:
+    """ This is the documentation of our class
+
+        Attributes:
+            _Square__size (int): The size of the square object
+    """
+    def __init__(self, size=0):
+        """ This is the initilizer of the class object
+
+            Args:
+                size (int): The size of the square
+            Raises:
+                TypeError: If the size is not an integer
+                ValueError: If the size is less than 0
+        """
+        self._Square__size = size
+
+    @property
+    def size(self):
+        """ Getter method for assigning the value to the size attribute
+
+            The setter method after this make sure that the size is an
+            integer and bigger than 0, otherwise it will raise either
+            a TypeError or a ValueError
+        """
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = value
+
+    def area(self):
+        """ Return the area of the current square object
+
+            Returns:
+                int: The area of the current square object created
+        """
+        return self._Square__size * self._Square__size
+
+    def my_print(self):
+        """ Prints in stdout the square using the '#' symbol
+
+            Returns: nothing, since it's just for writing on stdout
+        """
+        if self._Square__size == 0:
+            print()
+        for line in range(self._Square__size):
+            for columns in range(self._Square__size):
+                print("#", end="")
+            print('', end='\n')
