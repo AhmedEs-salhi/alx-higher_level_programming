592c0df9147d83fee8153d0323490385293cb0a1
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-08-06 15:35:23 +0100

    Let's welcome our first error :)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" This is the place where modules should be documented
+"""
+
+
+class Square:
+    """ This is the documentation of our class
+        
+        Attributes:
+            _Square__size (int): The size of the square object
+    """
+    def __init__(self, size):
+        """ This is the initilizer of the class object
+
+            Args:
+                size (int): The size of the square
+        """
+        self._Square__size = size
