2aaa2caf0c477db5de7f6e9b3c46c678e46ce1c5
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-08-08 17:48:23 +0100

    We're descovering new things

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+""" This is the place where modules should be documented
+"""
+
+
+class Square:
+    """ This is the documentation of our class
+
+        Attributes:
+            _Square__size (int): The size of the square object
+    """
+    def __init__(self, size=0):
+        """ This is the initilizer of the class object
+
+            Args:
+                size (int): The size of the square
+            Raises:
+                TypeError: If the size is not an integer
+                ValueError: If the size is less than 0
+        """
+        self._Square__size = size
+
+    @property
+    def size(self):
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise TypeError("size must be >= 0")
+        self._Square__size = value
+
+    def area(self):
+        """ Return the area of the current square object
+
+            Returns:
+                int: The area of the current square object created
+        """
+        return int(self._Square__size * self._Square__size)
+
+    def __eq__(self, other):
+        return self._Square__size == other._Square__size
+
+    def __lt__(self, other):
+        return self._Square__size < other._Square__size
+
+    def __gt__(self, other):
+        return self._Square__size > other._Square__size
+
+    def __ne__(self, other):
+        return self._Square__size != other._Square__size
+
+    def __le__(self, other):
+        return self._Square__size <= other._Square__size
+
+    def __ge__(self, other):
+        return self._Square__size >= other._Square__size
