53bee2bb4b64fd3eef7e641e648dbea4cddf2da4
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-08-08 19:05:24 +0100

    I was stuck a documentation :(

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
 import math
-""" Importing math module for using pi number in the mathematical calculatin
+""" Math module for some mathematical calculation.
+
+    Raises:
+        TypeError: In case the radius given is neither an integer
+        nor a float.
 """
 
 
 class MagicClass:
-    """ Class documentation
-        Containing several methods based on mathematical calculation
-        just to test the their bytecode
+    """ Class for translating the python bytecode
+
+        Attributes:
+            _MagicClass_radius: integer/float number containing the radius
     """
     def __init__(self, radius):
+        """ The Attribute initilizer
+
+        Args:
+            radius (int): Represent the radius given by the user
+
+        Raises:
+            TypeError: in case of the radius variable is neither an int
+            nor a float
+        """
         self._MagicClass_radius = 0
         if type(radius) is not int:
             if type(radius) is not float:
@@ -17,7 +31,17 @@ class MagicClass:
         self._MagicClass_radius = radius
 
     def area(self):
+        """ Return the area of the circle based on its radius
+
+        Returns:
+            float: the area of the circle
+        """
         return self._MagicClass_radius ** 2 * math.pi
 
     def circumference(self):
+        """ Return the boundries length of a circle
+
+        Returns:
+            float: The circumference calculated
+        """
         return 2 * math.pi * self._MagicClass_radius
