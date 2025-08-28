438e59ebc6286067c6ae3d5df7b5acc5908d93c6
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-08-28 16:18:04 +0100

    I actually don't know what is the problem

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,8 +13,8 @@ def add_integer(a, b=98):
         Return:
             The sum of a and b
     """
-    if type(a) not in [int, float]:
+    if type(a) not in [int, float] or a is None:
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if type(b) not in [int, float] or b is None:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
