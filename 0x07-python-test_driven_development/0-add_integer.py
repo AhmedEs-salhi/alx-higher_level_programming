99713f7e14a18c5f61d4a38f7b5e6b6ccea834ff
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-08-28 15:32:20 +0100

    documenting

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-
-
 """
     Adding integer module
 """
+
+
 def add_integer(a, b=98):
     """
         Adding integer function
+        Args:
+            a(int): The first integer
+            b(int): The second optional integer
+        Return:
+            The sum of a and b
     """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
