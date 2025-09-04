9f0bf9463210ebf3e960f7a1d4e485d874ac403d
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-09-05 00:14:25 +0100

    Well, this is soft

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This is module
+"""
+
+
+def text_indentation(text=None):
+    """ Function that prints a text with 2 new lines
+        after each of these characters: ., ? and :
+
+    Args:
+        text (str, optional): Text to be traited. Defaults to None.
+
+    Raises:
+        TypeError: Raises when the text is not an actual string
+    """
+    if type(text) is not str or text is None:
+        raise TypeError("text must be a string")
+    for ch in text:
+        if ch in ['.', ':', '?']:
+            print('\n', end='\n')
+            continue
+        print(ch, end='')
