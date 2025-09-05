39434855c3bf6e82b7811d3a16c5f0ba499a0df4
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-09-05 20:36:17 +0100

    Writing unit tests is really a pain in the ass

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,5 +8,46 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """ This is my class
     """
-    def test_max_integer(self):
+    def test_normal_max_integer(self):
+        """test for a noraml list of integers
+        """
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_max_integer_float(self):
+        """test for max at the beginning
+        """
+        self.assertEqual(max_integer([4.4, 1.2, 2.1, 3.8]), 4.4)
+        
+    def test_max_char_in_string(self):
+        """test for max character in a list of characters (string)
+        """
+        self.assertEqual(max_integer("Ahmedz"), 'z')
+        
+    def test_max_char_charList(self):
+        """_summary_
+        """
+        self.assertEqual(max_integer(["A", "z", "b", "c"]), 'z')
+
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
+        
+    def test_None_list(self):
+        self.assertRaises(TypeError, max_integer, None)
+    
+    def test_matrix(self):
+        self.assertRaises(TypeError, max_integer, [[12, 4, 5], 12, ['s', '2']])
+        
+    def test_negative_values(self):
+        self.assertEqual(max_integer([-1, -4, -9, -2]), -1)
+        
+    def test_one_element_list(self):
+        self.assertEqual(max_integer([9]), 9)
+    
+    def test_same_values(self):
+        self.assertEqual(max_integer([0, 0, 0, 0]), 0)
+        
+    def test_floats_and_ints(self):
+        self.assertEqual(max_integer([1, 3.2, 9, 0, 12.3]), 12.3)
+    
+    def test_NaN_list(self):
+        self.assertRaises(TypeError, max_integer, float('nan'))
