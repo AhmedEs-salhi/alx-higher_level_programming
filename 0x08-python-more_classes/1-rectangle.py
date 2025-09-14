c473409e719cf94da8e49be2f922bb5d1eec9ac2
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-09-14 18:37:30 +0100

    It's kind of a dumb mistakes start happening here

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -32,5 +32,5 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height mus be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
