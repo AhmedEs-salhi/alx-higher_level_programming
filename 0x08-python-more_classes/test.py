9b965498a95f73cf6f5c76a25ce8e23a66c2e311
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-09-15 23:52:15 +0100

    This was fun and dumb as well

--- 0x08-python-more_classes/test.py
+++ 0x08-python-more_classes/test.py
@@ -1,5 +1,12 @@
-import datetime
+def print_string(print_symbol='#'):
+    my_string = ''
+    for i in range(5):
+        for j in range(3):
+            my_string += str(print_symbol)
+        my_string += '\n'
+    return my_string[:-1]
 
-now = datetime.datetime.now()
-print(datetime.datetime(2025, 9, 15, 21, 42, 29, 834522))
-print(eval(repr(now)))
+my_string = "#"
+my_list = ['C', 'is fun']
+
+print(print_string(my_list))
