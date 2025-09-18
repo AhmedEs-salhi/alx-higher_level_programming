507a41b4746345af7b878c038d0f2dd28d43d7ef
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-09-18 13:56:50 +0100

    It's the damn documentation this time

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""I swear that this module is documented"""
+
+
 class LockedClass:
+    """I swear also that this class is documented as well"""
     __slots__ = ("first_name")
