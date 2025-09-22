0a96ea0527eeaf298a19c5fd28b21407a30c496e
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-09-22 23:07:03 +0100

    That was clever

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This is the module documentation
+"""
+
+
+def is_same_class(obj, a_class):
+    """ This is the method documentation
+    """
+    return obj.__class__.__name__ == a_class.__name__
