628086234cd075e17ae4e77b22e0b7bf53c3ca0d
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-09-23 00:12:49 +0100

    Hope this would be efficient even with less effort

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This is the module documentation
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ The function documentation
+    """
+    return issubclass(obj.__class__, a_class)
