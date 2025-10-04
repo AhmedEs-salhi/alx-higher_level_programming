a13e01a0d6fa3bdacd7bccb115f2b71b7294bdbe
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-10-04 18:38:06 +0100

    Let's drop another task

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This is the module documentation """
+import json
+
+
+def from_json_string(my_obj):
+    """ This is the function documentation """
+
+    return json.loads(my_obj)
