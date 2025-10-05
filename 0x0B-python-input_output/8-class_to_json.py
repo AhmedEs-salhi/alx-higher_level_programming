d7159cb5a2bee64a3a50bcdd002b2e00642f843b
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-10-05 18:04:19 +0100

    That was a genuis idea from me

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ This is the module documentation """
-import json
+from sys import stdout
+
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
 
 def class_to_json(obj):
     """ This is the function documentation """
-    return json.dumps(obj.__dict__)
+    save_to_json_file(obj.__dict__, "class.json")
+    with open("class.json", "r", encoding="utf-8") as file:
+        return file.read()
+
