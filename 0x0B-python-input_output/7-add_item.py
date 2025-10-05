103241a30f8cd42322f078cd7a4dc9e1607b2df3
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-10-05 17:37:21 +0100

    Let's try this out

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-
+""" This is the module documentation """
+import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-import sys
-import json
+
 
 def add_item(obj):
+    """ This is the function documentation """
     temp_obj = []
     for i in range(1, len(sys.argv)):
         temp_obj.append(sys.argv[i])
     try:
         obj = load_from_json_file("add_item.json")
     except Exception:
-
-        # Means that the file doesn't exist yet
-        # So I have to Create the file and either:
-            # Put "[]" inside (There is no arguments)
-            # Or the full list with argument appended (There is arguments)
-
         if len(sys.argv) != 0:
             obj += temp_obj
         save_to_json_file(obj, "add_item.json")
     else:
         obj += temp_obj
         save_to_json_file(obj, "add_item.json")
+
 
 add_item([])
