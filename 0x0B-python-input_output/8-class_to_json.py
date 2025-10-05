00ac81401bcb60d1eba8f9bcb2cea4fae2bc659f
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-10-05 18:08:34 +0100

    Actually it was dump. Let's not say dump but i overthinked about it

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 """ This is the module documentation """
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-
 
 def class_to_json(obj):
     """ This is the function documentation """
-    save_to_json_file(obj.__dict__, "class.json")
-    with open("class.json", "r", encoding="utf-8") as file:
-        return file.read()
-
+    return obj.__dict__
