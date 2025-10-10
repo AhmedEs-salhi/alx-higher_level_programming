f664778a3c6edc3b4dcf1a0b9e5ee45f2baa3894
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-10-10 16:41:28 +0100

    Let's make them work together

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,6 +9,14 @@ class Square(Rectangle):
         """ This is __init__ method documentation """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.getter
+    def size(self, size):
+        self.width = size
+
     def __str__(self):
         """ This is __str__ method documentation """
         return "[{}] ({}) {}/{} - {}".format(
