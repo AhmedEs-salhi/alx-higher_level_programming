d4e59c6e209c232f84dc1723241817d38b96ec55
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-10-10 16:35:22 +0100

    The same Duo; Documentation & pycodestyle checker

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,10 +6,12 @@ from .rectangle import Rectangle
 class Square(Rectangle):
     """ This is class documentation """
     def __init__(self, size, x=0, y=0, id=None):
+        """ This is __init__ method documentation """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """ This is __str__ method documentation """
         return "[{}] ({}) {}/{} - {}".format(
             self.__class__.__name__,
-            self.width, self.x, self.y, self.id
+            self.id, self.x, self.y, self.width
         )
