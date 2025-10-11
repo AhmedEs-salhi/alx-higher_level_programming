fdb7d79055ab434244a93e4862b869d70e0f79d3
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-10-11 18:54:51 +0100

    I think it's correct

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,4 +20,15 @@ class Base:
         """ This is to_json_string method documentation """
         if list_dictionaries:
             return json.dumps(list_dictionaries)
-        return "[]"
+        return '[]'
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        file_name = '{}.json'.format(cls.__name__)
+        list_dictionary = [obj.to_json_string(obj.to_dictionary()) for obj in list_objs]
+
+        with open(file_name, "w", encoding="utf-8") as json_file:
+            if type(list_objs) is None:
+                json.dump([], json_file)
+            else:
+                json.dump(list_dictionary, json_file)
