f8d79ee99e8403e69ab0c118c264154712e9deaa
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-10-12 15:32:19 +0100

    Once you build infrastructure, everything after becomes so easy to deal with

--- 0x0C-python-almost_a_circle/test.py
+++ 0x0C-python-almost_a_circle/test.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
-import json
-list_dict_json = []
-a_dictionary = {"name": "Ahmed", "age": 21, "CNE": 2229307}
-list_dict_json.append(a_dictionary)
-list_dict_json = json.dumps(list_dict_json)
-print(list_dict_json, type(list_dict_json))
 
+from models.rectangle import Rectangle
 
-def func(number):
-    if number:
-        return number
-    return "It's None you idiot"
-
-print(func(1024))
-print(func(None))
+r = Rectangle(10, 10)
+print(Rectangle.create())
