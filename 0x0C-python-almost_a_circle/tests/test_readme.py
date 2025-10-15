e1a7ba0ca59dd3ef03244bdb0a0772d988f9b588
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-10-15 02:35:43 +0100

    Let's try again

--- 0x0C-python-almost_a_circle/tests/test_readme.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-class TestReadme(unittest.TestCase):
-    def test_readme(self):
-        with open("README.md", "r", encoding="utf-8") as file:
-            size_read = file.read()
-        self.assertGreater(len(size_read), 0)
